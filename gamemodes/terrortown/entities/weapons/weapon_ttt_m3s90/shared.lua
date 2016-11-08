fb087dcded86414608f37010fff285d59e661f16
Marcel <zaratusa.codec@gmail.com>
2016-11-09 00:10:24 +0100

    M3S90 nerf
    - changed delay from 1.2 to 1.4
    - changed cone from 0.09 to 0.1
    - changed damage from 13 to 8
    - changed bullet amount from 8 to 13

--- gamemodes/terrortown/entities/weapons/weapon_ttt_m3s90/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_m3s90/shared.lua
@@ -23,16 +23,16 @@ SWEP.Base = "weapon_tttbase"
 
 --[[Default GMod values]]--
 SWEP.Primary.Ammo = "Buckshot"
-SWEP.Primary.Delay = 1.2
+SWEP.Primary.Delay = 1.4
 SWEP.Primary.Recoil	= 7
-SWEP.Primary.Cone = 0.09
-SWEP.Primary.Damage = 13
+SWEP.Primary.Cone = 0.1
+SWEP.Primary.Damage = 8
 SWEP.Primary.Automatic = true
 SWEP.Primary.ClipSize = 8
 SWEP.Primary.ClipMax = 24
 SWEP.Primary.DefaultClip = 8
 SWEP.Primary.Sound = Sound("Weapon_M3.Single")
-SWEP.Primary.NumShots = 8
+SWEP.Primary.NumShots = 13
 
 --[[Model settings]]--
 SWEP.HoldType = "shotgun"
